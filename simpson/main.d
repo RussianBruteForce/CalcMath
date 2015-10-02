7e4052913684cd28f31fd4aaf17bb9ddf114fbc7
Viktor Filinkov <bruteforce@sigil.tk>
2015-10-02 17:00:57 +0600

    Simpson's mathod. Writen in D.

--- simpson/main.d
+++ simpson/main.d
@@ -1,32 +1,46 @@
 import std.stdio;
 import std.math;
+import std.exception;
 
-class Simpson(RANGE_T) {
-	alias function_t = RANGE_T function(RANGE_T x);
-	function_t f;
-	RANGE_T h;
-	this (function_t f, const RANGE_T eps) {
-		assert(eps > 0);
+class Simpson(D_OH) {
+	alias function_t = D_OH function(D_OH x);
+	immutable function_t f;
+	immutable D_OH h;
+
+	this (function_t f, const D_OH eps) {
+		enforce(eps > 0, "I'm Bart Simpson. Who the hell are you?");//"Must be greater than 0!");
 		this.f = f;
 		this.h = pow(eps, 0.25);
-		writeln(this.h);
 	}
 
-	auto solve(const RANGE_T a, const RANGE_T b) {
-		assert(b > a);
-		auto n = ceil((b-a)/h);
-		writeln(n);
+	auto solve(const D_OH a, const D_OH b) const {
+		enforce(b > a, "Why, you litle!!11!1!");//"The second one must be greater than the first!");
+		auto n = ceil((b-a)/h); // computing n
+		while (n%4 != 0)
+			n++; // make n divisibile by 4 without rest
+		immutable auto local_h = (b-a)/n; // computing new h
+		D_OH S = f(a) + f(b); // init by sum of verges
+		// also, perhaps, should use loop without if statement, but with two variables
+		foreach(i; 1 .. n) {
+			immutable auto current = f(a+i*local_h);
+			if (i%2 == 0)
+				S += 2*current;
+			else
+				S += 4*current;
+		}
+		return S*local_h/3;
 	}
 }
 
 int main() {
-	enum eps = 0.001;
-	alias range_t = typeof(eps);
+	enum shorts = 0.0001;
+	alias range_t = typeof(shorts);
 
-	auto myF = function (range_t x) {return x*x;};
+	auto My = function (range_t x) {return x*x;}; // can't sovle сложно
 
-	auto simpson = new Simpson!range_t(myF, eps);
+	auto eat = new Simpson!range_t(My, shorts);
 
-	simpson.solve(0.6, 2.2);
-	return 1;
+	auto S = eat.solve(-6, 6.6);
+	writeln("Don't have a cow, man! Your definite integral is ", S, " c:");
+	return 0;
 }
